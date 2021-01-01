5f801870b4e75fdd046ba0b1bd5e81fe63790ae9
fabriceumuhire <fabrice.umuhire.ubugeni@gmail.com>
2021-01-02 00:55:19 +0200

    First Commit for React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+import TodoList from './components/ToDo';
+library.add(faTrash, faEdit, faPlus);
+// import Demo from "./components/Demo";
+// import Demo1 from "./components/Demo1";
+// import Parent from "./components/Parent";
+// import Sample from "./components/Sample";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList />
     </div>
   );
 }
